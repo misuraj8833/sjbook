2f416ceb18f1d690c30218cb2b24b38f2bece839
MishraFamily <mi.suraj8833@gmail.com>
2016-10-09 10:06:06 +0530

    with the add event successful and showevent unsuccessful

--- app/src/main/java/com/mishrafamily/sjbook/ShowEvent.java
+++ app/src/main/java/com/mishrafamily/sjbook/ShowEvent.java
@@ -2,11 +2,14 @@ package com.mishrafamily.sjbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.firebase.client.ChildEventListener;
@@ -14,6 +17,7 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.ui.FirebaseListAdapter;
+import com.firebase.ui.FirebaseRecyclerAdapter;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -22,8 +26,8 @@ import java.util.Map;
 
 public class ShowEvent extends AppCompatActivity {
     Firebase mref;
-    ListView listView;
-    String url = "https://sjbook-b02b6.firebaseio.com";
+    RecyclerView recyclerView;
+    String url = "https://sjbook-b02b6.firebaseio.com/Events";
 
 
     @Override
@@ -32,22 +36,37 @@ public class ShowEvent extends AppCompatActivity {
         setContentView(R.layout.activity_show_event);
         Firebase.setAndroidContext(this);
         mref = new Firebase(url);
-        listView=(ListView)findViewById(R.id.listViewEvents);
+        recyclerView=(RecyclerView)findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Firebase ref = mref.child("Events");
-        final FirebaseListAdapter<String> adapter = new FirebaseListAdapter<String>(this,String.class,R.layout.support_simple_spinner_dropdown_item,ref) {
+       // Firebase ref = mref.child("Events");
+        FirebaseRecyclerAdapter<String,EventViewHolder> firebaseRecyclerAdapter= new FirebaseRecyclerAdapter<String, EventViewHolder>(
+                String.class,
+                android.R.layout.two_line_list_item,
+                EventViewHolder.class,
+                mref
+        ) {
             @Override
-            protected void populateView(View view, String s, int i) {
-
-
-
+            protected void populateViewHolder(EventViewHolder eventViewHolder, String s, int i) {
+                eventViewHolder.textView.setText(s);
             }
         };
-        listView.setAdapter(adapter);
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+
     }
+
+   public class EventViewHolder extends RecyclerView.ViewHolder{
+       TextView textView;
+       public EventViewHolder(View itemView) {
+           super(itemView);
+           textView=(TextView)itemView.findViewById(android.R.id.text1);
+       }
+   }
 }
 
